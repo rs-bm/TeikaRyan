602214dbe86b3ee96b002a2b09c5ef1f4b1ffbf1
William Swanson <williamswanson@Williams-MacBook-Air.local>
2026-01-29 15:07:05 -0600

    Treat following player & creation of new objects
    I forgot to commit the first time

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine.InputSystem;
 public class PlayerBehavior : MonoBehaviour
 {
     public float speed; // amt of pixels moved per frame
+    public GameObject treat;
+    private GameObject currentTreat;
+    public float yOff = -1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +16,28 @@ public class PlayerBehavior : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+
+        if (currentTreat != null) {
+            // current player position
+            // gameObject is the owner of this script
+            Vector3 playerPos = transform.position;
+            Vector3 treatPos = new Vector3(0.0f, yOff, 0.0f);
+            currentTreat.transform.position = playerPos + treatPos;
+        } else
+        {
+            currentTreat = Instantiate(treat, new Vector3(0.0f, yOff, 0.0f), Quaternion.identity);
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame) {
+            // give treat gravity and enable collider
+            Rigidbody2D body = currentTreat.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+            Collider2D col = currentTreat.GetComponent<Collider2D>();
+    
+            col.enabled = true;
+            currentTreat = null;
+        }
+
         Keyboard k = Keyboard.current;
         if (k.leftArrowKey.isPressed || k.aKey.isPressed) {
             Vector3 newPos = transform.position;
