0fb4c22ba4f1627dab62ccb0a2f8d072af9fa47c
William Swanson <william.swanson@bruins.belmont.edu>
2026-02-05 15:13:42 -0600

    Bounding boxes in progress, Time.time experiements...

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -13,12 +13,12 @@ public class PlayerBehavior : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        float currentTime = Time.time;
+        print(currentTime);
     }
 
     // Update is called once per frame
     void Update() {
-
         if (currentTreat != null) {
             // current player position
             // gameObject is the owner of this script
